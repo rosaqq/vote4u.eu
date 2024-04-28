08af04bda45b485cf1f5c434aa6a1e90e4c58fcf
Tiago Rosa <tcrosa27@gmail.com>
2024-04-28 10:27:43 +0100

    reorganized party affiliations into list, local_party & group display candidate # before name

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,11 +1,11 @@
 export default function ListItem({ user }) {
-    const { first_name, last_name, bio, group, local_party, img_name, tags } = user;
+    const { pos, first_name, last_name, bio, list, local_party, group, img_name, tags } = user;
     const img_src = img_name ? `/img/ppl/${img_name}.jpg` : '';
     return (
         <article className="flex items-start space-x-6 p-6">
             <img src={img_src} alt="" width="60" height="88" className="flex-none rounded-md bg-slate-100" />
             <div className="min-w-0 relative flex-auto">
-                <h2 className="font-semibold text-slate-100 truncate pr-20">{first_name + " " + last_name}</h2>
+                <h2 className="font-semibold text-slate-100 truncate pr-20">{pos + ". " + first_name + " " + last_name}</h2>
                 <dl className="mt-2 flex flex-wrap text-sm leading-6 font-medium">
                     <div className="absolute top-0 right-0 flex items-center space-x-1">
                         <dt className="text-sky-500">
@@ -18,21 +18,21 @@ export default function ListItem({ user }) {
                     </div>
                     <div className="text-slate-200">
                         <dt className="sr-only">Rating</dt>
-                        <dd className="px-1.5 ring-1 ring-slate-600 rounded">{group}</dd>
+                        <dd className="px-1.5 ring-1 ring-slate-600 rounded">{list}</dd>
                     </div>
                     <div className="ml-2">
                         <dt className="sr-only">Year</dt>
                         <dd>{local_party}</dd>
                     </div>
-                    {/*<div>*/}
-                    {/*    <dt className="sr-only">Genre</dt>*/}
-                    {/*    <dd className="flex items-center">*/}
-                    {/*        <svg width="2" height="2" fill="currentColor" className="mx-2 text-slate-400" aria-hidden="true">*/}
-                    {/*            <circle cx="1" cy="1" r="1" />*/}
-                    {/*        </svg>*/}
-                    {/*        {movie.genre}*/}
-                    {/*    </dd>*/}
-                    {/*</div>*/}
+                    <div>
+                        <dt className="sr-only">Genre</dt>
+                        <dd className="flex items-center">
+                            <svg width="2" height="2" fill="currentColor" className="mx-2 text-slate-400" aria-hidden="true">
+                                <circle cx="1" cy="1" r="1" />
+                            </svg>
+                            {group}
+                        </dd>
+                    </div>
                     {/*<div>*/}
                     {/*    <dt className="sr-only">Runtime</dt>*/}
                     {/*    <dd className="flex items-center">*/}
