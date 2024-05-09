a7ca42d899dbf2d1189066dcd19ed137ca45f700
Tiago Rosa <tcrosa27@gmail.com>
2024-05-09 23:49:47 +0100

    code relative to the List block should be inside the list component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,9 @@
 export default function List({ children }) {
     return (
-        <ul className="divide-y divide-slate-200/5">
-            {children}
-        </ul>
+        <div className="relative z-10 rounded-xl shadow-xl ring-1 ring-slate-900/5 divide-y my-auto xl:mt-18 bg-slate-800 divide-slate-200/5 highlight-white/10">
+            <ul className="divide-y divide-slate-200/5">
+                {children}
+            </ul>
+        </div>
     )
 }
