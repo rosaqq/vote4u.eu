59230b0c3e2fcb0ac599a6bc6c2d2b493b9a9323
Tiago Rosa <tcrosa27@gmail.com>
2024-05-12 23:08:16 +0100

    Removed "..." as default bio text; removed unused code from ListItem

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -10,32 +10,19 @@ export default function ListItem({ user }) {
                 <h2 className="font-semibold text-slate-100 truncate">{pos + ". " + first_name + " " + last_name}</h2>
                 <dl className="mt-2 flex flex-wrap text-sm leading-6 font-medium">
 
+                    {/* National List */}
                     <div className="text-slate-200">
                         <dt className="sr-only">Partido</dt>
                         <dd className="px-1.5 ring-1 ring-slate-600 rounded">{local_party}</dd>
                     </div>
+
+                    {/* Europarl group */}
                     <div className="ml-2">
                         <dt className="sr-only">Grupo</dt>
                         <dd>{group}</dd>
                     </div>
-                    {/*<div>*/}
-                    {/*    <dt className="sr-only">Grupo</dt>*/}
-                    {/*    <dd className="flex items-center">*/}
-                    {/*        <svg width="2" height="2" fill="currentColor" className="mx-2 text-slate-400" aria-hidden="true">*/}
-                    {/*            <circle cx="1" cy="1" r="1" />*/}
-                    {/*        </svg>*/}
-                    {/*        {group}*/}
-                    {/*    </dd>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <dt className="sr-only">Runtime</dt>*/}
-                    {/*    <dd className="flex items-center">*/}
-                    {/*        <svg width="2" height="2" fill="currentColor" className="mx-2 text-slate-400" aria-hidden="true">*/}
-                    {/*            <circle cx="1" cy="1" r="1" />*/}
-                    {/*        </svg>*/}
-                    {/*        {movie.runtime}*/}
-                    {/*    </dd>*/}
-                    {/*</div>*/}
+
+                    {/* Bio */}
                     <div className="flex-none w-full mt-2 font-normal">
                         <dt className="sr-only">Bio</dt>
                         <dd className="text-slate-400">{bio}</dd>
