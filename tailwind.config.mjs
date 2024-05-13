b3f750e9df468f2c5dbc5c34db838fd7fdc37929
Tiago Rosa <tcrosa27@gmail.com>
2024-05-13 13:21:23 +0100

    Created component for the links with icons in the groupcards & now serving icons directly instead of loading BS-icons stylesheet. Closes #1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'45link': "url('/img/sym/link-45deg.svg')",
+				'boxlink': "url('/img/sym/box-arrow-up-right.svg')"
+			}
+		}
 	},
 	plugins: [],
 }
